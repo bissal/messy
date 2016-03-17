d8d60c468db97c1adcf3177b2133dc40764bc5c2
Gwak, Ok-Dong <gwak.okdong@gmail.com>
2016-03-17 17:19:55 +0900

    add HelloExamples.java temporarily

---
+++ examples/src/main/java/HelloExamples.java
@@ -0,0 +1,8 @@
+/**
+ * Created by 4our6ix on 2016. 3. 17..
+ */
+public class HelloExamples {
+    public static void main(String[] args) {
+        System.out.println("Hello, examples");
+    }
+}
